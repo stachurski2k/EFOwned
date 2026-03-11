ced051dd42ff0f93be303580764afa83ab50e802
Stanisław Deja <staskacp412@gmail.com>
2026-03-11 11:26:49 +0100

    Db configuration

--- src/EFOwned/EFOwned/Program.cs
+++ src/EFOwned/EFOwned/Program.cs
@@ -1,5 +1,10 @@
+using EFOwned.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<MainDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
